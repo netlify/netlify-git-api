cc181d922d8c3ab65592a36a21367cfe08f91858
Mathias Biilmann Christensen <info@mathias-biilmann.net>
2015-06-30 15:04:25 -0700

    Go 1.3 compatibility

--- api/api.go
+++ api/api.go
@@ -2,6 +2,9 @@ package api
 
 import (
 	"net/http"
+	"strings"
+
+	"encoding/base64"
 
 	"github.com/julienschmidt/httprouter"
 	"github.com/netlify/netlify-git-api/repo"
@@ -50,7 +53,13 @@ func (a *API) tokenFn() httprouter.Handle {
 			return
 		}
 
-		email, pw, ok := r.BasicAuth()
+		auth := r.Header.Get("Authorization")
+		if auth == "" {
+			NotAuthorizedError(w, "No authorization header")
+			return
+		}
+
+		email, pw, ok := parseBasicAuth(auth)
 		if !ok {
 			NotAuthorizedError(w, "Missing email or password")
 			return
@@ -102,4 +111,24 @@ func NewAPI(resolver Resolver) http.Handler {
 	})
 
 	return corsHandler.Handler(router)
+}
+
+// From go 1.4 request implementation
+// parseBasicAuth parses an HTTP Basic Authentication string.
+// "Basic QWxhZGRpbjpvcGVuIHNlc2FtZQ==" returns ("Aladdin", "open sesame", true).
+func parseBasicAuth(auth string) (username, password string, ok bool) {
+	const prefix = "Basic "
+	if !strings.HasPrefix(auth, prefix) {
+		return
+	}
+	c, err := base64.StdEncoding.DecodeString(auth[len(prefix):])
+	if err != nil {
+		return
+	}
+	cs := string(c)
+	s := strings.IndexByte(cs, ':')
+	if s < 0 {
+		return
+	}
+	return cs[:s], cs[s+1:], true
 }
